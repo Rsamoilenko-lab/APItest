0b3873f52cf2eae15e22b87460dfabf5e2fdf8fe
Rsamoilenko-lab <rsamoilenko@octogames.studio>
2025-04-04 15:11:50 +0300

    Update expectedResponses.js

--- src/data/expectedResponses.js
+++ src/data/expectedResponses.js
@@ -6,7 +6,7 @@ export default class ExpectedResponse {
         language: 'en',
         level: { lastPointsUsed: 0, level: 0, points: 0, shopPoints: 0 },
         result: true,
-        timezone: '',
+        timezone: "Africa/Abidjan",
         userId: 11591,
         messaging__login: '92__apo-ross_kingbilly.xyz',
         messaging__host: 'wss://centric.cetrifugo-host.net'
